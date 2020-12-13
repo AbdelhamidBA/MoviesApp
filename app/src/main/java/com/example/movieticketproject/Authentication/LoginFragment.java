3c775006984aeea2056784cfc1635cc1ce5d4108
AbdelhamidBA <benabdelfettah.abdelhamid@gmail.com>
2020-12-13 15:27:03 +0100

    12/13/2020

--- app/src/main/java/com/example/movieticketproject/Authentication/LoginFragment.java
+++ app/src/main/java/com/example/movieticketproject/Authentication/LoginFragment.java
@@ -1,7 +1,11 @@
 package com.example.movieticketproject.Authentication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -14,9 +18,20 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Switch;
 
+import com.example.movieticketproject.HomeActivity;
 import com.example.movieticketproject.MainActivity;
+import com.example.movieticketproject.Models.Users;
 import com.example.movieticketproject.R;
 import com.example.movieticketproject.SplashScreen;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class LoginFragment extends Fragment {
@@ -24,6 +39,9 @@ public class LoginFragment extends Fragment {
     SwitchCompat rememberme;
     Button loginBTN;
     ImageView back;
+    DatabaseReference reference = null;
+    String email,password;
+    SharedPreferences pref;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -34,7 +52,10 @@ public class LoginFragment extends Fragment {
         loginBTN = v.findViewById(R.id.btn_login_login);
         rememberme = v.findViewById(R.id.sw_remember_login);
         back = v.findViewById(R.id.ic_back_login);
-
+        reference = FirebaseDatabase.getInstance().getReference().child("Users");
+        email = emailED.getText().toString();
+        password = passED.getText().toString();
+        pref = getActivity().getSharedPreferences("current_user", Context.MODE_PRIVATE);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,7 +65,65 @@ public class LoginFragment extends Fragment {
                 fragmentTransaction.commit();
             }
         });
+        loginBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(emailED.getText().toString().equals("") || !isEmailValid(emailED.getText().toString()))
+                {
+                    emailED.setError("Enter a valid Email");
+                }
+                else if(passED.getText().toString().equals("") || passED.getText().toString().length() < 6)
+                {
+                    passED.setError("Enter a valid password");
+                }
+                else
+                {
+                    Query query = reference.orderByChild("email").equalTo(emailED.getText().toString());
+                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.exists())
+                            {
+                                String passwordDB = snapshot.child(email).child("password").getValue(String.class);
+                                if(passwordDB.equals(password))
+                                {
+                                    Users currentUser = snapshot.child(email).getValue(Users.class);
+                                    SharedPreferences.Editor editor = pref.edit();
+                                    editor.putString("fullname",currentUser.getFullname());
+                                    editor.putString("email",currentUser.getEmail());
+                                    editor.putString("role",currentUser.getRole());
+                                    editor.commit();
+                                    if(currentUser.getRole() == "client")
+                                    {
+                                        Intent intent = new Intent(LoginFragment.this.getActivity(), HomeActivity.class);
+                                        getActivity().startActivity(intent);
+                                        getActivity().finishActivity(0);
+                                    }
+                                }
+                                else
+                                {
+                                    passED.setError("Please Check Your Password");
+                                }
+                            }
+                            else {
+                                emailED.setError("Email does not exist");
+                            }
+                    }
 
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+             }
+            }
+        });
         return v;
+    }
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 }
---
+++ app/src/main/java/com/example/movieticketproject/Models/Users.java
@@ -0,0 +1,61 @@
+package com.example.movieticketproject.Models;
+
+public class Users {
+    String fullname;
+    String birthdate;
+    String email;
+    String password;
+    String avatarPic;
+    String role;
+
+    public Users() {
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatarPic() {
+        return avatarPic;
+    }
+
+    public void setAvatarPic(String avatarPic) {
+        this.avatarPic = avatarPic;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
